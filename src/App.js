e7afb1ebb15b417d2b05446b3db8988b35b24ebf
CarlW <carl@oskarroger.net>
2019-09-12 14:57:34 +0800

    wrapped App with the ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io'
 });
 
-function App() {
-  return <div>Hello from React.</div>;
-}
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Hello from React.</h2>
+      </div>
+    </ApolloProvider>
+  );
+};
 
 export default App;
